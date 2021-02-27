c24c8748c2f7bede48cdcd2ca8b26929c4e83115
izzyevermore <isaiahlekay2001@gmail.com>
2021-02-27 12:43:29 +0200

    update

--- assets/script/new.js
+++ assets/script/new.js
@@ -1,35 +1,9 @@
-let users;
+ // Admilet users;n
+ let admin;
 
-fetch('https://murmuring-anchorage-60741.herokuapp.com/show-students/')
-.then(res => res.json())
-.then(data => {
-    console.log(data);
-    users = data;
-})
-
-function login(){
-    let inputs = document.getElementsByTagName('input');
-    
-    let username = inputs[0].value;
-    let password = inputs[1].value;
-
-console.log(users, username, password);
-
-    let loggedIn = users.filter(user => {
-        return user.username == username && user.password == password
-    })
-
-     if(loggedIn.length >= 1){
-        alert('User Found');
-        window.location.href = './landing.html'
-     } else {
-         alert('Credential Invalid')
-     }
-}
-
-fetch('https://murmuring-anchorage-60741.herokuapp.com/show-admin/',)
-.then(res => res.json())
-.then(data => {
+     fetch('https://murmuring-anchorage-60741.herokuapp.com/show-admin/',)
+    .then(res => res.json())
+    .then(data => {
     console.log(data);
     admin = data;
 })
@@ -55,6 +29,38 @@ console.log(admin, username, password);
         alert('Credentials Invalid')
     }
 }
+
+    let users;
+
+fetch('https://murmuring-anchorage-60741.herokuapp.com/show-students/')
+.then(res => res.json())
+.then(data => {
+    console.log(data);
+    users = data;
+})
+
+function login(){
+    
+    let inputs = document.getElementsByTagName('input');
+
+    let username  = inputs[0].value;
+    let password = inputs[1].value;
+
+    let loggedIn = users.filter(user => {
+        return user.username == username && user.password == password
+    })
+
+     if(loggedIn){
+        alert('User Found');
+        window.location.href = './landing.html'
+     } else {
+         alert('Credential Invalid')
+     }}
+
+
+
+
+
 
 
 
